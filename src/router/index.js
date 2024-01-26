45abffd0b067ad22af4f90d81ea8dc9eff1b534a
Callay <2415993100@qq.com>
2024-01-26 14:00:43 +0800

    路由守卫与验证码错误处理

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: Callay 2415993100@qq.com
  * @Date: 2024-01-09 21:39:03
  * @LastEditors: Callay 2415993100@qq.com
- * @LastEditTime: 2024-01-14 00:46:49
+ * @LastEditTime: 2024-01-26 13:55:36
  * @FilePath: \vue\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -59,7 +59,9 @@ router.beforeEach((to,from,next)=>{
       next({path:'/index'})
     }
   }else if(to.path=='/register'){
-      next()
+    if(isLogin!=null){
+      next({path:'/index'})
+    }
   }else if(isLogin == null){
     next({path:'/login'});
   }
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -2,7 +2,7 @@
  * @Author: Callay 2415993100@qq.com
  * @Date: 2024-01-13 17:28:16
  * @LastEditors: Callay 2415993100@qq.com
- * @LastEditTime: 2024-01-14 00:59:13
+ * @LastEditTime: 2024-01-26 13:56:58
  * @FilePath: \vue\src\views\RegisterView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -156,10 +156,16 @@ export default {
                 }
             })
         },
+        //获取验证码
         getCode() {
             if (this.user.email) {
                 this.$request.get('/user/getcode?email=' + this.user.email).then(res => {
-                    sessionStorage.setItem("verification-code",res.data)
+                    if(res.code === 200){
+                        sessionStorage.setItem("verification-code",res.data)
+                    }else{
+                        this.$message.error(res.msg)
+                    }
+                    
                 })
             } else {
                 this.$notify({

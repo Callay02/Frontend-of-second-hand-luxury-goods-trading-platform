394d95d7aa0ffbcebf01a15a92c8671f0229ee02
Callay <2415993100@qq.com>
2024-02-17 21:59:24 +0800

    实现阿里云OSS图片上传，图片路径还需修改

---
+++ src/utils/alioss.js
@@ -0,0 +1,8 @@
+let OSS = require('ali-oss')
+export let client = new OSS({
+  region: 'oss-cn-hangzhou',
+  accessKeyId: 'LTAI5tDqLBMKXMws6CkuGZNh',
+  accessKeySecret: '8sxryFQXWDFSoseavjzIPxID1gjA6J',
+  bucket: 'callay-spdb'
+})
+

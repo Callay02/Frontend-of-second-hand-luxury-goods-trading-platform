3905868b3967dad90427df8377543a2ccd8e3950
Callay <2415993100@qq.com>
2024-04-17 00:06:25 +0800

    修改网页标签标题和icon

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
  * @Author: Callay 2415993100@qq.com
  * @Date: 2024-01-09 21:39:03
  * @LastEditors: Callay 2415993100@qq.com
- * @LastEditTime: 2024-02-07 21:26:53
+ * @LastEditTime: 2024-04-17 00:05:20
  * @FilePath: \vue\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style>
-</style>
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: Callay 2415993100@qq.com
  * @Date: 2024-02-16 23:57:03
  * @LastEditors: Callay 2415993100@qq.com
- * @LastEditTime: 2024-04-09 14:53:49
+ * @LastEditTime: 2024-04-16 23:55:53
  * @FilePath: \vue\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -482,6 +482,7 @@ VueRouter.prototype.push = function push(location) {
 
 //登录跳转
 router.beforeEach((to,from,next)=>{
+  window.document.title = to.meta.title == undefined ? '二手奢侈品交易平台' : to.meta.title
   let isLogin = sessionStorage.getItem("satoken");
   
   //登出
